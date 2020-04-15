0bf972ee43ebe40b4cee57d771aec9e0877cd975
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-15 14:27:11 -0400

    Update anticor_anticor.m

--- Strategy/anticor_anticor.m
+++ Strategy/anticor_anticor.m
@@ -2,14 +2,21 @@ function [cum_ret, cumprod_ret, daily_ret, daily_portfolio] ...
     = anticor_anticor(fid, data, varargins, opts)
 % This file is an entry for the BAH(Anticor(Anticor)) strategy, the best
 % variants of anticor
+% 此文件为BAH, 长期持有策略的子入口文件。
 %
 % function [cum_ret, cumprod_ret, daily_ret, daily_portfolio] ...
-%           = anticor_anticor_start(fid, data, varargins, opts)
+%           = anticor(fid, data, varargins, opts)
+%
+% 函数所用输入变量分别为：
+% cum_ret, cumprod_ret, daily_ret, daily_portfolio
+% 最终回报，累积回报，每日回报，每日资产组合。
+%
 % cum_ret: cumulative wealth achived at the end of a period.
 % cumprod_ret: cumulative wealth achieved till the end each period.
 % daily_ret: daily return achieved by a strategy.
 % daily_portfolio: daily portfolios
 %
+% 使用数据变量：市场序列矢量，日志书写，变量参数，为行为性操作预留的选择性输入参数。
 % data: market sequence vectors
 % fid: handle for write log file
 % varargins: variable parameters
