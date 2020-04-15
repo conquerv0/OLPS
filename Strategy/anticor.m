10db72658d45f64a64f8d9632b74075eb328ed75
Victor Xiao <58013002+conquerv0@users.noreply.github.com>
2020-04-15 14:15:01 -0400

    Update anticor.m

--- Strategy/anticor.m
+++ Strategy/anticor.m
@@ -1,16 +1,22 @@
 function [cum_ret, cumprod_ret, daily_ret, daily_portfolio] ...
     = anticor(fid, data, varargins, opts)
 % This file is an entry for the BAH(Anticor) strategy, the second best
-% variants of anticor
+% variants of anticor 
+% 此文件为BAH, 长期持有策略的子入口文件。
 %
 % function [cum_ret, cumprod_ret, daily_ret, daily_portfolio] ...
 %           = anticor(fid, data, varargins, opts)
+%
+% 函数所用输入变量分别为：
+% cum_ret, cumprod_ret, daily_ret, daily_portfolio
+% 最终回报，累积回报，每日回报，每日资产组合。
 %
 % cum_ret: cumulative wealth achived at the end of a period.
 % cumprod_ret: cumulative wealth achieved till the end each period.
 % daily_ret: daily return achieved by a strategy.
 % daily_portfolio: daily portfolios
 %
+% 使用数据变量：市场序列矢量，日志书写，变量参数，为行为性操作预留的选择性输入参数。
 % data: market sequence vectors
 % fid: handle for write log file
 % varargins: variable parameters
@@ -28,8 +34,8 @@ function [cum_ret, cumprod_ret, daily_ret, daily_portfolio] ...
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Extract the parameters
-W  = varargins{1};      % Maximum window size, or the number of experts
-tc = varargins{2};      % transaction cost fee rate
+W  = varargins{1};      % Maximum window size, or the number of experts | 最大化交易区间，或是专家数量
+tc = varargins{2};      % transaction cost fee rate | 生成交易费用
 
 % Run the anticor_run algorithm
 [cum_ret, cumprod_ret, daily_ret, daily_portfolio]...
